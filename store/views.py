5085999e1d2f627fa2e6b63b791b9d5cef39876c
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-04-23 20:04:50 +0300

    Create some new permissions, fix error

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
-from store.permissins import IsOwnerOrStaffOrReadOnly
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BookSerializer
 
 
